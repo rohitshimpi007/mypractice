8b07c89a2b38edc78f05c78de8880482cc15b4a9
rohitshimpi007 <shimpirohit007@gmail.com>
2023-11-24 14:58:33 +0530

    material form owner login

--- hotel-managment-angular/src/app/owner/owner.module.ts
+++ hotel-managment-angular/src/app/owner/owner.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { OwnerRoutingModule } from './owner-routing.module';
 import { OwnerLoginComponent } from './owner-login/owner-login.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { OwnerLoginComponent } from './owner-login/owner-login.component';
   ],
   imports: [
     CommonModule,
-    OwnerRoutingModule
+    OwnerRoutingModule,
+    SharedModule
   ]
 })
 export class OwnerModule { }
